94fd043c68f96cbe70cbb070201ebc4081a9f9f1
harithamadana <harithamadana@gmail.com>
2025-03-15 22:28:46 +0530

    Initial Commit

---
+++ App/music_player/lib/core/configs/theme/app_theme.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:music_player/core/configs/theme/app_colors.dart';
+
+class AppTheme {
+  static final lightTheme = ThemeData(
+    primaryColor: AppColors.primary,
+    scaffoldBackgroundColor: AppColors.lightBackground,
+    brightness: Brightness.light,
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+
+        backgroundColor: AppColors.primary,
+        textStyle: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(24)
+        )
+      )
+    )
+  );
+}
